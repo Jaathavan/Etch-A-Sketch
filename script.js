942a7079839a8b6942ed34a57a73c49345f1b09c
Jaathavan <rjaathu@outlook.com>
2022-06-16 19:48:38 -0400

    density readjustable

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ function createGrid(num) {
     gridLayout.style.setProperty('--grid', num)
     for (let i=0; i < (num**2); i++) {
         let gridBox = document.createElement("div");
-        gridBox.textContent = (i+1);
         gridLayout.appendChild(gridBox).className = "grid-item";
-        console.log("Work");
     }
 }
 
