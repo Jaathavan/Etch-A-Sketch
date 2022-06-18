7301153679fbfff53d20eaa5efd6a9c5c9e400aa
Jaathavan <rjaathu@outlook.com>
2022-06-17 21:35:40 -0400

    working sketch function

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 const gridLayout = document.getElementById('grid-container');
 
+let mouseDown = false;
+
+document.body.onmousedown = () => {
+    mouseDown = true;
+};
+
+document.body.onmouseup = () => {
+    mouseDown = false;
+  };
+
 function createGrid(num) {
     gridLayout.style.setProperty('--grid', num)
     for (let i=0; i < (num**2); i++) {
         let gridBox = document.createElement("div");
         gridLayout.appendChild(gridBox).className = "grid-item";
+        gridBox.addEventListener('mouseover', () => {
+            if (mouseDown === true) {
+                gridBox.style.backgroundColor = 'black';
+            }
+        })
+        gridBox.addEventListener('mousedown', () => {
+            gridBox.style.backgroundColor = 'black';
+        })
     }
 }
 
 createGrid(22);
-
-const grids = document.querySelectorAll('grid-item');
-
-grids.style.backgroundColor ='green'
-
-grids.forEach((g) => {
-    g.addEventListener('mousedown', () => {
-        g.style.backgroundColor = 'black';
-    })
-})
