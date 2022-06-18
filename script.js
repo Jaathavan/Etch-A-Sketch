f13a49cc6719ec99163af143f1a71f34b9b237e7
Jaathavan <rjaathu@outlook.com>
2022-06-18 19:25:13 -0400

    will allow slider to change square density

--- script.js
+++ script.js
@@ -26,25 +26,35 @@ function createGrid(num) {
     }
 }
 
-createGrid(16)
+createGrid(16);
 
-const rangeInputs = document.querySelectorAll('input[type="range"]')
-const numberInput = document.querySelector('input[type="number"]')
+const rangeInputs = document.querySelectorAll('input[type="range"]');
+const numberInput = document.querySelector('input[type="number"]');
 
 function handleInputChange(e) {
-  let target = e.target
+  let target = e.target;
   if (e.target.type !== 'range') {
-    target = document.getElementById('range')
+    target = document.getElementById('range');
   } 
-  const min = target.min
-  const max = target.max
-  const val = target.value
+  const min = target.min;
+  const max = target.max;
+  const val = target.value;
   
-  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+  const gridItems = document.querySelectorAll('.grid-item');
+
+  if (gridItems.length > 0) {
+    gridItems.forEach (g => {
+        g.remove();
+    })
+  }
+  
+  createGrid(val);
+
+  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%';
 }
 
 rangeInputs.forEach(input => {
-  input.addEventListener('input', handleInputChange)
+  input.addEventListener('input', handleInputChange);
 })
 
-numberInput.addEventListener('input', handleInputChange)
+numberInput.addEventListener('input', handleInputChange);
